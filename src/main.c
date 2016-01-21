40faf954eef87ae71e144fe28c053c26eb402319
David Balland <morveus@morveus.com>
2016-01-21 20:58:45 +0100

    Tests
    blah

--- src/main.c
+++ src/main.c
@@ -1,22 +1,127 @@
-#include <pebble.h>
+#include "pebble.h"
 
-Window *my_window;
-TextLayer *text_layer;
+#define NUM_MENU_SECTIONS 2
+#define NUM_MENU_ICONS 3
+#define NUM_FIRST_MENU_ITEMS 3
+#define NUM_SECOND_MENU_ITEMS 1
 
-void handle_init(void) {
-  my_window = window_create();
+static Window *s_main_window;
+static MenuLayer *s_menu_layer;
 
-  text_layer = text_layer_create(GRect(0, 0, 144, 20));
-  window_stack_push(my_window, true);
+static uint16_t menu_get_num_sections_callback(MenuLayer *menu_layer, void *data) {
+  return NUM_MENU_SECTIONS;
 }
 
-void handle_deinit(void) {
-  text_layer_destroy(text_layer);
-  window_destroy(my_window);
+static uint16_t menu_get_num_rows_callback(MenuLayer *menu_layer, uint16_t section_index, void *data) {
+  switch (section_index) {
+    case 0:
+      return NUM_FIRST_MENU_ITEMS;
+    case 1:
+      return NUM_SECOND_MENU_ITEMS;
+    default:
+      return 0;
+  }
+}
+
+static int16_t menu_get_header_height_callback(MenuLayer *menu_layer, uint16_t section_index, void *data) {
+  return MENU_CELL_BASIC_HEADER_HEIGHT;
+}
+
+static void menu_draw_header_callback(GContext* ctx, const Layer *cell_layer, uint16_t section_index, void *data) {
+  // Determine which section we're working with
+  switch (section_index) {
+    case 0:
+      // Draw title text in the section header
+      menu_cell_basic_header_draw(ctx, cell_layer, "Some example items");
+      break;
+    case 1:
+      menu_cell_basic_header_draw(ctx, cell_layer, "One more");
+      break;
+  }
+}
+
+static void menu_draw_row_callback(GContext* ctx, const Layer *cell_layer, MenuIndex *cell_index, void *data) {
+  // Determine which section we're going to draw in
+  switch (cell_index->section) {
+    case 0:
+      // Use the row to specify which item we'll draw
+      switch (cell_index->row) {
+        case 0:
+          // This is a basic menu item with a title and subtitle
+          menu_cell_basic_draw(ctx, cell_layer, "Basic Item", "With a subtitle", NULL);
+          break;
+        case 1:
+          // This is a basic menu icon with a cycling icon
+          menu_cell_basic_draw(ctx, cell_layer, "Icon Item", "Select to cycle", NULL);
+          break;
+      }
+      break;
+    case 1:
+      switch (cell_index->row) {
+        case 0:
+          // There is title draw for something more simple than a basic menu item
+          menu_cell_title_draw(ctx, cell_layer, "Final Item");
+          break;
+      }
+  }
+}
+
+static void menu_select_callback(MenuLayer *menu_layer, MenuIndex *cell_index, void *data) {
+  // Use the row to specify which item will receive the select action
+  switch (cell_index->row) {
+    // This is the menu item with the cycling icon
+    case 1:
+      // Cycle the icon
+      //s_current_icon = (s_current_icon + 1) % NUM_MENU_ICONS;
+      // After changing the icon, mark the layer to have it updated
+      layer_mark_dirty(menu_layer_get_layer(menu_layer));
+      break;
+  }
+
+}
+
+static void main_window_load(Window *window) {
+  // Now we prepare to initialize the menu layer
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_frame(window_layer);
+
+  // Create the menu layer
+  s_menu_layer = menu_layer_create(bounds);
+  menu_layer_set_callbacks(s_menu_layer, NULL, (MenuLayerCallbacks){
+    .get_num_sections = menu_get_num_sections_callback,
+    .get_num_rows = menu_get_num_rows_callback,
+    .get_header_height = menu_get_header_height_callback,
+    .draw_header = menu_draw_header_callback,
+    .draw_row = menu_draw_row_callback,
+    .select_click = menu_select_callback,
+  });
+
+  // Bind the menu layer's click config provider to the window for interactivity
+  menu_layer_set_click_config_onto_window(s_menu_layer, window);
+
+  layer_add_child(window_layer, menu_layer_get_layer(s_menu_layer));
+}
+
+static void main_window_unload(Window *window) {
+  // Destroy the menu layer
+  menu_layer_destroy(s_menu_layer);
+}
+
+static void init() {
+  s_main_window = window_create();
+  window_set_window_handlers(s_main_window, (WindowHandlers) {
+    .load = main_window_load,
+    .unload = main_window_unload,
+  });
+  window_stack_push(s_main_window, true);
+}
+
+static void deinit() {
+  window_destroy(s_main_window);
 }
 
 int main(void) {
-  handle_init();
+  init();
   app_event_loop();
-  handle_deinit();
+  deinit();
 }
